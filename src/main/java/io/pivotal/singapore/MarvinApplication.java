f390600e664779b7cfd2b90a4baec69d1c28597a
Pair <pair@pivotal.io>
2016-03-17 14:48:36 +0800

    Initial commit

---
+++ src/main/java/io/pivotal/singapore/MarvinApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.singapore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarvinApplication.class, args);
+    }
+}
