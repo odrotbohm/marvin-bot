920d612445f9700d61201d52bba55a901447b717
Björn Andersson <ba@sanitarium.se>
2016-03-21 23:27:18 +0800

    Fix broken timezone conversion
    Damn timezones. Different behaviors depending on whether Date from
    PrettyTimeParser contains a timezone or not.
    
    [#115919549]

--- src/main/java/io/pivotal/singapore/services/ArgumentParserService.java
+++ src/main/java/io/pivotal/singapore/services/ArgumentParserService.java
@@ -5,9 +5,14 @@ import org.ocpsoft.prettytime.nlp.parse.DateGroup;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.*;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -58,14 +63,37 @@ class ArgumentParserService {
         return results.group(1);
     }
 
+    // TODO: Maybe not hardcode in Singapore here?
     private String parseTimestamp(String s) {
         List<DateGroup> parse = new PrettyTimeParser().parseSyntax(s);
         if (parse != null && !parse.isEmpty()) {
             Date d = parse.get(0).getDates().get(0);
 
-            // TODO: Maybe not hardcode in Singapore here?
-            LocalDateTime localDate = LocalDateTime.ofEpochSecond(d.getTime() / 1000, 0, ZoneOffset.of("+08:00"));
-            return ZonedDateTime.of(localDate, ZoneOffset.of("+08:00")).format(ISO_OFFSET_DATE_TIME);
+            /* $)(&*%)(@# Timezones.
+             * The original timezone offset is only available from calendar instances.
+             *
+             * NOTE: The paths taken by this code is very different depending on your local
+             * timezone. If you intend to change it at least try it out in two very different zones.
+             * Current testing has been done using TZ=UTC and TZ=Asia/Singapore.
+             *
+             * Definitions:
+             *   display time: The visible time, e.g. 19:00 +00:00. Ignoring the timezone the
+             *                 time displayed is correct.
+             *   correct time: The underlying time(stamp) is correct, but it's displayed with the
+             *                 wrong timezone. E.g. 11:00 +00:00 which should be 19:00 +08:00.
+             */
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.setTime(d);
+            ZonedDateTime zonedDateTime = calendar
+                .toInstant()
+                .atZone(ZoneOffset.of("+00:00")); // Forces the timezone to UTC for *all*
+            if (calendar.getTimeZone().getRawOffset() == 0) { // Correct display time, wrong time
+                zonedDateTime = zonedDateTime.withZoneSameLocal(ZoneId.of("+08:00"));
+            } else { // Correct time, wrong display time
+                zonedDateTime = zonedDateTime.withZoneSameInstant(ZoneId.of("+08:00"));
+            }
+
+            return zonedDateTime.format(ISO_OFFSET_DATE_TIME);
         } else {
             return null;
         }
