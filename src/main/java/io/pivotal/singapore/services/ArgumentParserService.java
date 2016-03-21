cafbafca703e06fb01e7c76e3ffd7debe6a99d35
Björn Andersson + Michael Cheng + Abhaya Shenoy <pair+ba+mc+as@pivotal.io>
2016-03-21 19:15:36 +0800

    Add regex parsing to ArgumentParserService
    [#115919549]

--- src/main/java/io/pivotal/singapore/services/ArgumentParserService.java
+++ src/main/java/io/pivotal/singapore/services/ArgumentParserService.java
@@ -6,23 +6,68 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
 
 @Service
 class ArgumentParserService {
 
-    Optional parseTimestamp(String s) {
+    TreeMap parse(String s, TreeMap<String, String> argumentsConfig) {
+        TreeMap<String, String> returnMap = new TreeMap<>();
+
+        for (Map.Entry<String, String> e : argumentsConfig.entrySet()) {
+            String regex = e.getValue();
+
+            if (regex.startsWith("/")) {
+                try {
+                    String match = parseRegex(s, regex);
+                    s = s.replace(match, "");
+                    returnMap.put(e.getKey(), match);
+                } catch (IndexOutOfBoundsException ex) {
+                    throw new IllegalArgumentException(
+                        String.format("Argument '%s' found no match with regex '%s'", e.getKey(), regex),
+                        ex
+                    );
+                }
+            } else if (regex.equals("TIMESTAMP")) {
+                returnMap.put(e.getKey(), parseTimestamp(s));
+            } else {
+                throw new IllegalArgumentException(
+                    String.format("The argument '%s' for '%s' is not valid", regex, e.getKey())
+                );
+            }
+        }
+
+        return returnMap;
+    }
+
+    private String parseRegex(String s, String regex) {
+        regex = (String) regex.subSequence(1, regex.length() - 1);
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(s);
+        m.find();
+        MatchResult results = m.toMatchResult();
+
+        return results.group(1);
+    }
+
+    private String parseTimestamp(String s) {
         List<DateGroup> parse = new PrettyTimeParser().parseSyntax(s);
         if (parse != null && !parse.isEmpty()) {
             Date d = parse.get(0).getDates().get(0);
 
             // TODO: Maybe not hardcode in Singapore here?
-            return Optional.of(LocalDateTime.ofEpochSecond(d.getTime() / 1000, 0, ZoneOffset.of("+08:00")));
+            LocalDateTime localDate = LocalDateTime.ofEpochSecond(d.getTime() / 1000, 0, ZoneOffset.of("+08:00"));
+            return ZonedDateTime.of(localDate, ZoneOffset.of("+08:00")).format(ISO_OFFSET_DATE_TIME);
         } else {
-            return Optional.empty();
+            return null;
         }
     }
+
 }
--- src/test/java/io/pivotal/singapore/services/ArgumentParserServiceTest.java
+++ src/test/java/io/pivotal/singapore/services/ArgumentParserServiceTest.java
@@ -2,68 +2,110 @@ package io.pivotal.singapore.services;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.time.*;
+import java.util.*;
 
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Enclosed.class)
 public class ArgumentParserServiceTest {
+    private static class BaseTest {
 
-    private ArgumentParserService aps = new ArgumentParserService();
-    private LocalDateTime expectedDateTime;
+        ArgumentParserService aps = new ArgumentParserService();
+        TreeMap<String, String> argumentsConfig;
+        String expectedDateTimeString;
+        ZonedDateTime expectedDateTime;
 
-    @Before
-    public void setup() {
-        expectedDateTime = LocalDateTime.of(LocalDate.of(2016, 3, 23), LocalTime.of(19, 0));
+        @Before
+        public void setup() {
+            expectedDateTime = ZonedDateTime.of(LocalDate.of(2016, 3, 23), LocalTime.of(19, 0), ZoneId.of("+08:00"));
+            expectedDateTimeString = expectedDateTime.format(ISO_OFFSET_DATE_TIME);
+
+            argumentsConfig = new TreeMap<>();
+            argumentsConfig.put("timestamp", "TIMESTAMP");
+        }
     }
 
-    @Test
-    public void parseTimestringOnItsOwn() {
-        Optional actual = aps.parseTimestamp("23rd of March at 7pm");
+    @RunWith(MockitoJUnitRunner.class)
+    public static class TimeParsing extends BaseTest {
 
-        assertThat(actual.isPresent(), is(true));
-        assertThat(actual.get(), equalTo(expectedDateTime));
+        @Test
+        public void parseTimestringOnItsOwn() {
+            TreeMap actual = aps.parse("23rd of March at 7pm", argumentsConfig);
+
+            assertThat(actual.get("timestamp"), equalTo(expectedDateTimeString));
+        }
+
+        @Test
+        public void noValidTimeString() {
+            TreeMap actual = aps.parse("I'm a fluffy ballonicorn!", argumentsConfig);
+
+            assertThat(actual.get("timestamp") == null, is(true));
+        }
+
+        @Test
+        public void parseValidTimeStringWithOtherStuff() {
+            TreeMap actual = aps.parse("BBQ At the Pivotal Labs Singapore office on the 23rd of March at 7pm", argumentsConfig);
+
+            assertThat(actual.get("timestamp"), equalTo(expectedDateTimeString));
+        }
+
+        @Test
+        public void parseAllSupportedTimestrings() {
+            List<String> validStrings = Arrays.asList(
+                "on the 23rd of March at 7pm",
+                "at 7pm on the 23rd of March",
+                "23 March 7pm",
+                "2016-03-23 7pm",
+                "at 19:00 March 23",
+                "next thursday at 9pm"
+            );
+
+            for (String validString : validStrings) {
+                assertThat(aps.parse(validString, argumentsConfig).get("timestamp") != null, is(true));
+            }
+        }
     }
 
-    @Test
-    public void noValidTimeString() {
-        Optional actual = aps.parseTimestamp("I'm a fluffy ballonicorn!");
+    @RunWith(MockitoJUnitRunner.class)
+    public static class ParseArgument extends BaseTest {
+        @Test
+        public void parseStringWithArguments() {
+            String s = "\"BBQ At the Pivotal Labs Singapore office\" on the 23rd of March at 7pm";
+            TreeMap<String, String> argumentsConfig = new TreeMap<>();
+            argumentsConfig.put("event_name", "/([^\"]+)/");
+            argumentsConfig.put("start_time", "TIMESTAMP");
 
-        assertThat(actual.isPresent(), is(false));
-    }
+            TreeMap result = aps.parse(s, argumentsConfig);
 
-    @Test
-    public void parseValidTimeStringWithOtherStuff() {
-        Optional actual = aps.parseTimestamp("BBQ At the Pivotal Labs Singapore office on the 23rd of March at 7pm");
+            assertThat(result.get("event_name"), equalTo("BBQ At the Pivotal Labs Singapore office"));
+            assertThat(result.get("start_time"), equalTo(expectedDateTimeString));
+        }
 
-        assertThat(actual.isPresent(), is(true));
-        assertThat(actual.get(), equalTo(expectedDateTime));
-    }
+        @Test(expected = IllegalArgumentException.class)
+        public void parseStringWithInvalidArgumentsRaisesException() {
+            TreeMap<String, String> argumentsConfig = new TreeMap<>();
+            argumentsConfig.put("event_name", "m000");
 
-    @Test
-    public void parseAllSupportedTimestrings() {
-        List<String> validStrings = Arrays.asList(
-            "on the 23rd of March at 7pm",
-            "at 7pm on the 23rd of March",
-            "23 March 7pm",
-            "2016-03-23 7pm",
-            "at 19:00 March 23",
-            "next thursday at 9pm"
-        );
-        
-        for (String validString : validStrings) {
-            assertThat(aps.parseTimestamp(validString).isPresent(), is(true));
+            aps.parse("", argumentsConfig);
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void raisesExceptionWhenNotAllPartsMatches() {
+            String s = "Hello 123 there";
+            TreeMap<String, String> arguments = new TreeMap<>();
+            arguments.put("first", "/\\w+/");
+            arguments.put("second", "/\\w+/");
+            arguments.put("third", "/\\w+/");
+
+            TreeMap result = aps.parse(s, arguments);
         }
     }
 }
