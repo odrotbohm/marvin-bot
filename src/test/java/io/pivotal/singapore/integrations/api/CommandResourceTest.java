5a4b3c3b7340113b1497c589b001a538c3c3495d
Swayam Narain + Alan Yeo <pair+sn+ay@pivotal.io>
2016-03-22 10:33:08 +0800

    Use RestAssured for API integration tests
    [#115917679]

--- src/test/java/io/pivotal/singapore/integrations/api/CommandResourceTest.java
+++ src/test/java/io/pivotal/singapore/integrations/api/CommandResourceTest.java
@@ -1,62 +1,76 @@
 package io.pivotal.singapore.integrations.api;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 import io.pivotal.singapore.MarvinApplication;
 import io.pivotal.singapore.models.Command;
 import io.pivotal.singapore.repositories.CommandRepository;
 import org.json.JSONObject;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MarvinApplication.class)
 @WebAppConfiguration
 @ActiveProfiles(profiles = "test")
+@IntegrationTest("server.port:0")
+
 public class CommandResourceTest {
     @Autowired private WebApplicationContext wac;
     @Autowired private CommandRepository commandRepository;
+
+    @Value("${local.server.port}")
+    protected int port;
 
     private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
+        RestAssured.port = port;
         mockMvc = webAppContextSetup(wac).build();
     }
 
-    @Transactional
+    @After
+    public void tearDown() {
+        commandRepository.deleteAll();
+    }
+
     @Test
-    @Transactional
     public void testCommandsListing() throws Exception {
         commandRepository.save(new Command("cmd1", "http://localhost/1"));
         commandRepository.save(new Command("cmd2", "http://localhost/2"));
 
-        mockMvc.perform(get("/api/v1/commands"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaTypes.HAL_JSON))
-                .andExpect(jsonPath("$._embedded.commands").isArray())
-                .andExpect(jsonPath("$._embedded.commands.[0].name", is("cmd1")))
-                .andExpect(jsonPath("$._embedded.commands.[0].endpoint", is("http://localhost/1")))
-                .andExpect(jsonPath("$._embedded.commands.[1].name", is("cmd2")))
-                .andExpect(jsonPath("$._embedded.commands.[1].endpoint", is("http://localhost/2")));
+        when().
+                get("/api/v1/commands").
+        then().
+                statusCode(SC_OK).
+                contentType(MediaTypes.HAL_JSON_VALUE).
+                body("_embedded.commands.size()", is(2)).
+                body("_embedded.commands[0].name", is("cmd1")).
+                body("_embedded.commands[0].endpoint", is("http://localhost/1")).
+                body("_embedded.commands[1].name", is("cmd2")).
+                body("_embedded.commands[1].endpoint", is("http://localhost/2"));
     }
 
-    @Transactional
     @Test
     public void testCommandsCreation() throws Exception {
         String json = new JSONObject()
@@ -65,14 +79,50 @@ public class CommandResourceTest {
                 .put("method", "GET")
                 .toString();
 
-        mockMvc.perform(post("/api/v1/commands/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
-                .andExpect(status().isCreated());
+        given().
+                contentType(ContentType.JSON).
+                content(json).
+        when().
+                post("/api/v1/commands/").
+        then().
+                statusCode(SC_CREATED);
 
-        mockMvc.perform(get("/api/v1/commands"))
-                .andExpect(jsonPath("$._embedded.commands").isArray())
-                .andExpect(jsonPath("$._embedded.commands.[0].name", is("command")))
-                .andExpect(jsonPath("$._embedded.commands.[0].endpoint", is("http://localhost/9")));
+        when().
+                get("/api/v1/commands").
+        then().
+                body("_embedded.commands.size()", is(1)).
+                body("_embedded.commands[0].name", is("command")).
+                body("_embedded.commands[0].endpoint", is("http://localhost/9"));
+    }
+
+    @Test
+    public void testCommandsUpdateViaName() throws Exception {
+        JSONObject originalJson = new JSONObject()
+                .put("name", "command")
+                .put("endpoint", "http://localhost/9")
+                .put("method", "GET");
+
+        given().
+                contentType(ContentType.JSON).
+                content(originalJson.toString()).
+        when().
+                post("/api/v1/commands/").
+        then().
+                statusCode(SC_CREATED);
+
+        JSONObject newJson = originalJson.put("method", "POST");
+
+        given().
+                contentType(ContentType.JSON).
+                content(newJson.toString()).
+        when().
+                post("/api/v1/commands/").
+        then().
+                statusCode(SC_CONFLICT);
+
+        when().
+                get("/api/v1/commands/").
+        then().
+                content("page.totalElements", equalTo(1));
     }
 }
